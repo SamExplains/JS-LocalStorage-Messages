bd5b0c2adc03a8810708b711de6543ba5da6cedb
Sampuccino <Sampuccino@gmail.com>
2018-08-21 17:46:31 -0700

    Initial commit with starter files.

---
+++ Index.php
@@ -0,0 +1,14 @@
+<?php include "resources/includes/header.php" ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-10 mx-auto m-3">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, earum facere iste itaque nostrum
+        perferendis repellat vero? Ad beatae commodi delectus ex quidem soluta ut. Amet beatae dolor iure laudantium
+        magnam minus pariatur sapiente voluptatum. Aliquid cupiditate deserunt dignissimos, distinctio eveniet excepturi
+        hic illo nesciunt non nostrum omnis rem tenetur.</p>
+    </div>
+  </div>
+</div>
+
+<?php include "resources/includes/footer.php"; ?>
---
+++ resources/includes/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Local Storage Messenger</title>
+
+  <!--Bootstrap 4-->
+  <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
+  <!--Animate CSS-->
+  <link rel="stylesheet" href="./resources/css/animate.css">
+  <!--Custom CSS-->
+  <link rel="stylesheet" href="./resources/css/style.css">
+
+</head>
+
+<body>
+
+
