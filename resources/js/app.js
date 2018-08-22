6102b24f6bc559cbe0aed385f08308c5f13b4149
Sampuccino <Sampuccino@gmail.com>
2018-08-21 21:00:47 -0700

    Finish writing functions to store messages locally. Just need to finish grabbing them from local storage.

--- resources/js/app.js
+++ resources/js/app.js
@@ -34,6 +34,9 @@ const messageList = $('#message-list');
 
     messageList.append(data);
 
+    /*Add to local storage */
+    addMessageToLocalStorage(inputMessage);
+
   }
 
   /* Remove message from DOM */
@@ -48,4 +51,30 @@ const messageList = $('#message-list');
     } else {
       console.warn('Class not found');
     }
+  }
+
+  function addMessageToLocalStorage(message) {
+    console.log('Local Storage Function with parameter value ' + message);
+    let msg = retrieveMessageFromStorage();
+    console.log(msg);
+
+    //Add message into array
+    msg.push(message);
+
+    //Convert array to string
+    localStorage.setItem('msg', JSON.stringify(msg));
+
+  }
+
+  function retrieveMessageFromStorage(){
+    let msg;
+    const messagesLS = localStorage.getItem('msg');
+    /* Get values but if they are null we crate and empty array to house them */
+    if( messagesLS === null ) {
+      msg = [];
+    } else  {
+      msg = JSON.parse(messagesLS);
+    }
+
+    return msg;
   }
