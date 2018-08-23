07bf94a32e533c7d6a72b30b88ee1b565849d5c6
Sampuccino <Sampuccino@gmail.com>
2018-08-23 09:56:18 -0700

    Did some cleaning up, form reset and end of project.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,7 @@ if (localStorage.getItem('index')){
   console.error('OUR INDEX IS UNDEFINED!');
   indexCount = localStorage.getItem('index').length;
 } else {
-  //Retreive localstorage array size
-  //indexCount = localStorage.getItem('index').length;
   indexCount = 0;
-  //console.error(indexCount);
 }
 
 const messageList = $('#message-list');
@@ -25,6 +22,7 @@ const messageList = $('#message-list');
 
     /* Document Ready */
     document.addEventListener('DOMContentLoaded',localStorageOnLoad);
+
   }
 
 
@@ -32,12 +30,8 @@ const messageList = $('#message-list');
   function newMessage(e) {
     e.preventDefault();
 
-    console.warn('Form Submitted');
-
     /* Get the value that the user typed */
     const inputMessage = $("#userMessage").val();
-
-    console.warn(inputMessage);
 
     /* Remove Button */
     const removeButton = `<img src="./resources/img/delete.svg" width="20" alt="">`;
@@ -52,44 +46,32 @@ const messageList = $('#message-list');
     /*Add to local storage */
     addMessageToLocalStorage(inputMessage, indexCount);
     indexCount++;
-
+    /* Reset textarea to blank */
+    $("#userMessage").val("");
   }
 
   /* Remove message from DOM */
   function removeMessage(e) {
-    //console.log(e.delegateTarget.childNodes[1].classList.contains("remove-message"));
 
-    // if (e.target.classList.contains("remove-message")){
     if (e.delegateTarget.childNodes[1].classList.contains("remove-message")){
-      console.warn('This contains the .remove-message class.');
-      console.warn('___ ID FOR THIS TABLE ROW IS : #' + clickedRowID);
-      //console.error(e.delegateTarget.childNodes);
+
       /* Grab Element To Be Deleted */
       let element = document.getElementById(clickedRowID);
-      // console.log(element);
-      /* WORKING BUT ... Newly added items won't be deleted. Try a refresh! */
+
       element.remove();
-      //element.parentNode.removeChild(clickedRowID);
-      //e.delegateTarget.childNodes[1].remove();
 
       /* Remove item(s) from local storage */
       //Grab the text content of the item
       const messageText = element.firstElementChild.textContent;
-      //console.log(messageText);
 
       removeMessageFromLocalStorage(messageText);
 
-    } else {
-      console.warn('Class not found');
     }
   }
 
   function addMessageToLocalStorage(message, idx) {
-    console.log('Local Storage Function with parameter value ' + message + ' With ID of ' + idx);
     let msg = retrieveMessageFromStorage();
     let index = retrieveIndexFromStorage();
-
-    console.log(msg + ' ID OF ' + index.length);
 
     //Add message into array
     index.push(idx);
@@ -130,7 +112,6 @@ function retrieveIndexFromStorage(){
   function localStorageOnLoad() {
     let messages = retrieveMessageFromStorage();
     let idx = retrieveIndexFromStorage();
-    // console.warn(messages);
 
     let count = 0;
     messages.forEach(function (msg) {
@@ -160,17 +141,10 @@ function retrieveIndexFromStorage(){
 
     msg.forEach(function (mLS, index) {
       if(elemDelete === mLS) {
-        console.warn('WE HAVE A MATCH');
-        console.warn('Removing ' + elemDelete + ' from local storage . . .');
         msg.splice(index, 1);
         idx.splice(clickedRowID, 1);
-      } else {
-        //console.warn('NO MATCH FOUND, Elem is ' + elemDelete + ' ' + elemDelete.length + ' Local Storage was ' + mLS + ' ' + mLS.length );
       }
     });
-    console.error('NEW Local Storage Message Array Is ' + msg);
-    console.error('NEW Local Storage INDEX Array Is ' + idx);
     localStorage.setItem('msg', JSON.stringify(msg));
     localStorage.setItem('index', JSON.stringify(idx));
-
   }
